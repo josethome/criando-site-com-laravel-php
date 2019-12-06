c78c99c25a65f6a1fa562e124d6b8117c542d43b
Jose Thome <josethome.band@hotmail.com>
2019-12-06 19:40:27 -0200

    passando dados por um formulário

---
+++ app/Http/Controllers/usuariosController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class usuariosController extends Controller
+{
+    // apresentar o formulário
+    public function show() {
+    	return view('formulario');
+    }
+
+    // tratar os dados do formulário
+    public function tratar(Request $dados) {
+    	return 'Cheguei!';		
+    }
+}
