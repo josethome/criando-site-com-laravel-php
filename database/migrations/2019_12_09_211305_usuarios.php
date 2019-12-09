23b0611f7c74d0b0548ec81ea6d29777968db95d
Jose Thome <josethome.band@hotmail.com>
2019-12-09 19:46:10 -0200

    criação de tabelas testes como forma de fixação dos comandos

---
+++ database/migrations/2019_12_09_211305_usuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Usuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('usuario', 80);
+            $table->string('senha', 64);
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
