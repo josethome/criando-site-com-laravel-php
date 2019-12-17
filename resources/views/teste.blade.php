c7a6a26b91c9a38449fc215c37b43beda6610da9
Jose Thome <josethome.band@hotmail.com>
2019-12-17 00:42:05 -0200

    adicionando a biblioteca bootstrap v4.0.0 no laravel

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('conteudo')
+
+	<h1>Título</h1>
+	<p>Este é o texto de um parágrafo.</p>
+	<button class="btn btn-outline-primary btn-lg" type="submit">Button</button>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('teste', function() {
+	return view('teste');
+});
+
 //Route::resource('noticias', 'noticiasController');
 
 //Route::get('noticias/edit/{id}', 'noticiasController@edit');
 
-Route::get('noticias/update/{id}', 'noticiasController@update');
+//Route::get('noticias/update/{id}', 'noticiasController@update');
 
-Route::get('noticias/delete/{id}', 'noticiasController@destroy');
+//Route::get('noticias/delete/{id}', 'noticiasController@destroy');
