1a0eb5db8c808334926383e56bef4e0ba96c644f
Jose Thome <josethome.band@hotmail.com>
2019-12-17 12:59:32 -0200

    apresentação da grid conteúdo notícias

--- resources/views/noticias_index.blade.php
+++ resources/views/noticias_index.blade.php
@@ -4,11 +4,43 @@
 @section('conteudo')
 	
 	@if (count($dados) == 0)
+
 		{{-- Não existem notícias --}}
 		<p class="alert alert-danger text-center">Não foram encontradas notícias</p>
-	@else 
-		{{-- Apresentar as notícias existentes na base de dados --}}	
 
+	@else 
+
+		{{-- Apresentar as notícias existentes na base de dados --}}	
+		
+		@foreach ($dados as $noticia)
+			
+			{{-- Cabeçalho da notícia (título, autor, data-hora) --}}
+			<div class="row bg-success">
+
+				{{-- Título --}}
+				<div class="col-md-9">
+					<h2>{{ $noticia->titulo }}</h2>	
+				</div>
+
+				{{-- Autor data e hora do registro  --}}
+				<div class="col-md-3 text-right" style="padding-top: 25px">
+					<span class="glyphicon glyphicon-pencil">						
+					</span> {{ $noticia->autor }} 
+					<span class="glyphicon glyphicon-time">
+					</span> {{ $noticia->updated_at }}
+				</div>				
+			</div>
+
+			{{-- Contéudo da notícia --}}
+			<div class="row">
+				<div class="col-md-12" style="padding-top: 10px">
+					{{ $noticia->texto }}
+				</div>
+			</div>
+
+			{{-- Separador --}}
+			<hr>
+		@endforeach
 	@endif
 
 @endsection
